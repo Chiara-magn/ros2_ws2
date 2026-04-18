ad0e141583f30d8a72035cc7315806696a7b20d1
Chiara-magn <chiara.magnasco03@gmail.com>
2026-04-18 09:10:31 +0000

    Action modified, initialized include in robot_nav_server.cpp

---
+++ src/robot_nav/src/robot_nav_server.cpp
@@ -0,0 +1,10 @@
+#include <memory>
+#include <thread>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
+#include "robot_nav/action/move_to_pose.hpp"
+
